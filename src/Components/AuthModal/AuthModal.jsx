72542606be8400b211754c79283a53d26399f6e9
Aca M <61030878+axcamz@users.noreply.github.com>
2022-11-12 11:08:54 +0700

    [FIX] background in auth modal

--- src/Components/AuthModal/AuthModal.jsx
+++ src/Components/AuthModal/AuthModal.jsx
@@ -10,10 +10,10 @@ import './AuthModal_style.css'
 const AuthModal = () => {
   const navigate = useNavigate()
   return (
-    <div className='modal-wrapper background'>
-      <div className='trigger' onClick={() => navigate(-1)}>
-        <LoginModal/>
+    <div className='modal-wrapper'>
+      <div className='trigger background' onClick={() => navigate(-1)}>
       </div>
+      <LoginModal/>
     </div>
   )
 }
