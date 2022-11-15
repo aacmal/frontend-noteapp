55c3ddba9c900b695d556553a2e151b45aa28cd8
Aca M <61030878+axcamz@users.noreply.github.com>
2022-11-15 18:50:25 +0700

    Deleted some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import MainRouter from './router/index'
 
 import './style.css'
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_HOST_DEV = 'http://localhost:3001';
 const API_PRODUCTION_URL = 'https://backend-noteapp.acmal.me'
-const API_ROOT_PATH = '/note';
 export const BASE_URL = `${API_PRODUCTION_URL}`;
 
 export const instance = axios.create({
